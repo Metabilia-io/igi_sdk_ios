f83677094caa201df0844e362a929a52b9db7cce
hammadmetabilia <hmehmood@igotitapp.com>
2024-01-25 01:22:44 +0500

    name refactoring

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "IGIPackage",
+    name: "igi_sdk",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "IGIPackage",
+            name: "igi_sdk",
             targets: ["igi_sdk"]),
     ],
     targets: [
