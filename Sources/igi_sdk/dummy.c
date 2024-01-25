6fb237b90a1eea9b674e1c5c52c10fa1c0209c67
hammadmetabilia <hmehmood@igotitapp.com>
2024-01-25 18:26:49 +0500

    Package dependencies issue fixed

--- Sources/igi_sdk/dummy.c
+++ Sources/igi_sdk/dummy.c
@@ -1,9 +1,8 @@
 //
-//  File.swift
+//  dummy.c
 //  
 //
 //  Created by Hammad Mehmood on 25/01/2024.
 //
 
-import Foundation
-import igi_sdk
+#include "dummy.h"
---
+++ Sources/igi_sdk/dummy.h
@@ -0,0 +1,13 @@
+//
+//  dummy.h
+//  
+//
+//  Created by Hammad Mehmood on 25/01/2024.
+//
+
+#ifndef dummy_h
+#define dummy_h
+
+#include <stdio.h>
+
+#endif /* dummy_h */
